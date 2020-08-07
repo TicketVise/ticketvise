4e3d7b2d13fd00b73e18d9082ef992fef81985ba
Ruben Jonk <ruben.jonk@hotmail.nl>
2020-08-07 17:54:15 +0200

    added vue searchbar

---
+++ frontend/components/elements/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <card class="flex" outlined>
+      <i class="fa fa-search cursor-pointer p-1"></i>
+      <input class="flex-grow m-2" v-model="search" placeholder="Search" @keyup="emitValue">
+    </card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props: ["value"],
+  data() {
+    return {
+      search: "",
+    }
+  },
+  methods: {
+    emitValue() {
+      this.$emit('input', this.search)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/components/ticket_overview/TicketOverview.vue
+++ frontend/components/ticket_overview/TicketOverview.vue
@@ -1,37 +1,53 @@
 <template>
-  <div class="flex space-x-6 flex-grow">
-    <!-- Banner -->
+  <div>
+    <div class="flex mb-2">
+      <search-bar v-on:input="get_tickets" class="flex-grow"></search-bar>
+    </div>
+    <div class="flex space-x-6 flex-grow">
+      <!-- Banner -->
 
-    <!-- Sorteren -->
+      <!-- Sorteren -->
 
-    <!-- Columns -->
-    <ticket-column
-      v-for="(column, i) in tickets"
-      :key="column.label"
-      :title="column.label"
-      :color="colors[i]"
-      :ticket-list="column.tickets"
-    />
+      <!-- Columns -->
+      <ticket-column
+          v-for="(column, i) in tickets"
+          :key="column.label"
+          :title="column.label"
+          :color="colors[i]"
+          :ticket-list="column.tickets"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import SearchBar from "../elements/SearchBar";
+
 export default {
+  components: {SearchBar},
   props: ['title'],
   data: () => ({
     colors: ['#e76f51', '#e9c46a', '#2a9d8f', '#264653'],
-    tickets: []
+    tickets: [],
+    debounce_search: null
   }),
+  methods: {
+    get_tickets(search) {
+      window.axios.get(`/api${window.location.pathname}`, {
+        params: {
+          columns: true,
+          q: search
+        }
+      }).then(response => {
+        this.tickets = response.data
+      })
+    },
+    search_tickets(search) {
+      this.debounce_search(search)
+    }
+  },
   created() {
-    const self = this
-    
-    window.axios.get(`/api${window.location.pathname}`, {
-      params: {
-        columns: true
-      }
-    }).then(response => {
-      self.tickets = response.data
-    })
+    this.get_tickets()
   }
 }
 </script>
