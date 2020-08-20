5cc3c4224f13560adb24eecded8d6b429e4494cf
Ruben Jonk <ruben.jonk@hotmail.nl>
2020-08-20 17:52:29 +0200

    page number

--- frontend/components/notifications/Notifications.vue
+++ frontend/components/notifications/Notifications.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="container divide-y-2 divide-y-m-2 divide-gray-400">
+    <div class="container divide-y divide-gray-100">
         <nav class="m-3">
-                <div class="divide-x divide-gray-400 flex">
-                    <span @click="toggleAll" class="font-semibold text-center flex-grow" v-if="read === ''">All</span>
-                    <a @click="toggleAll" class="text-center flex-grow text-blue-500" v-else>All</a>
+            <div class="divide-x divide-gray-400 flex">
+                <span @click="toggleAll" class="font-semibold text-center flex-grow" v-if="read === ''">All</span>
+                <a @click="toggleAll" class="text-center flex-grow text-blue-500" v-else>All</a>
 
-                    <span @click="toggleRead" class="text-center font-semibold flex-grow" v-if="read === 'True'">Read</span>
-                    <a @click="toggleRead" class="text-center flex-grow text-blue-500" v-else>Read</a>
+                <span @click="toggleRead" class="text-center font-semibold flex-grow" v-if="read === 'True'">Read</span>
+                <a @click="toggleRead" class="text-center flex-grow text-blue-500" v-else>Read</a>
 
-                    <span @click="toggleUnread" class="text-center font-semibold flex-grow" v-if="read === 'False'">Unread</span>
-                    <a @click="toggleUnread" class="text-center flex-grow text-blue-500" v-else>Unread</a>
+                <span @click="toggleUnread" class="text-center font-semibold flex-grow"
+                      v-if="read === 'False'">Unread</span>
+                <a @click="toggleUnread" class="text-center flex-grow text-blue-500" v-else>Unread</a>
 
-                    <submit-button text="Mark all as read" v-on:click.native="markAllAsRead"
-                                   class="bg-orange-400 flex-wrap md:flex-wrap-0"></submit-button>
-                </div>
+                <submit-button text="Mark all as read" v-on:click.native="markAllAsRead"
+                               class="bg-orange-400 flex-wrap md:flex-wrap-0"></submit-button>
+            </div>
         </nav>
         <notification-card v-for="notification in notifications.results" :key="notification.id"
                            :notification="notification"></notification-card>
-        <submit-button v-on:click.native=prevPage() text="Prev" class="m-2"
-                       v-if="notifications.previous"></submit-button>
-        <submit-button v-on:click.native=nextPage() text="Next" class="m-2" v-if="notifications.next"></submit-button>
+        <div class="flex justify-end w-full">
+            <submit-button v-on:click.native=prevPage() text="Prev" class="m-2"
+                           v-if="notifications.previous"></submit-button>
+            <span v-if="notifications.next || notifications.previous">{{pageNumber}}</span>
+            <submit-button v-on:click.native=nextPage() text="Next" class="m-2"
+                           v-if="notifications.next"></submit-button>
+        </div>
     </div>
 </template>
 
@@ -37,6 +42,7 @@
                 notifications: [],
                 search: "",
                 read: "",
+                pageNumber: 1
             }
         },
         created() {
@@ -47,16 +53,17 @@
         methods: {
             nextPage() {
                 if (this.notifications.next) {
-                    let page_num = this.notifications.next.substring(this.notifications.next.indexOf("=") + 1);
-                    axios.get("/api/notifications", {params: {page: page_num}}).then(response => {
+                    this.pageNumber = this.notifications.next.substring(this.notifications.next.indexOf("=") + 1);
+                    axios.get("/api/notifications", {params: {page: this.pageNumber}}).then(response => {
                         this.notifications = response.data
                     })
                 }
             },
             prevPage() {
                 if (this.notifications.previous) {
-                    let page_num = this.notifications.previous.substring(0, this.notifications.previous.indexOf("="));
-                    if (!page_num) {
+                    this.pageNumber = this.notifications.previous.substring(0, this.notifications.previous.indexOf("="));
+                    if (!this.pageNumber) {
+                        this.pageNumber = 1
                         axios.get("/api/notifications").then(response => {
                             this.notifications = response.data
                         })
