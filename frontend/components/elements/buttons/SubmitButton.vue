c054d8249b3f1fb7b3ed24ff8699849a08159b58
IvandeWolf <ivan.dewolf1997@gmail.com>
2020-08-23 23:53:04 +0200

    Small changes

--- frontend/components/elements/buttons/SubmitButton.vue
+++ frontend/components/elements/buttons/SubmitButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="$emit('click')" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-orange hover:bg-orange-500 transition duration-150 ease-in-out">
+    <button @click="$emit('click')" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-orange hover:bg-orange-500 hover:text-white transition duration-150 ease-in-out">
         <slot></slot>
         {{text}}
     </button>
