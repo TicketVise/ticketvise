6dfe0dc4202ad6937a50f2f093fa50ba871d7804
rubenjonk <68845414+rubenjonk@users.noreply.github.com>
2020-08-24 22:19:51 +0300

    Labels missing (#93)
    * parent-child communication
    
    * added placeholder
    
    * change assignee
    
    * remove items from dropdown
    
    * remove right item from dropdown

--- frontend/components/elements/dropdown/LabelDropdown.vue
+++ frontend/components/elements/dropdown/LabelDropdown.vue
@@ -1,13 +1,9 @@
 <template>
-  <!-- <select v-on:input="updateValue($event.target.value)"
-          class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-      <option>Add label</option>
-      <option :value="item.id" v-for="item in values" :key="item.id">{{item.name}}</option>
-  </select> -->
   <div class="space-y-1">
     <div class="relative">
                 <span class="inline-block w-full rounded-md shadow-sm" @click="open = !open">
-                    <button aria-expanded="true" aria-haspopup="listbox" aria-labelledby="listbox-label" class="cursor-pointer relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                    <button aria-expanded="true" aria-haspopup="listbox" aria-labelledby="listbox-label"
+                            class="cursor-pointer relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                             tabindex="-1"
                             type="button">
                         <div class="flex items-center space-x-3">
@@ -22,23 +18,27 @@
                     </button>
                 </span>
 
-      <div class="absolute mt-1 w-full rounded-md bg-white shadow-lg" v-if="open">
-        <ul aria-activedescendant="listbox-item-3" aria-labelledby="listbox-label" class="max-h-56 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5" role="listbox"
+      <div class="absolute mt-1 w-full rounded-md bg-white shadow-lg z-50" v-if="open">
+        <ul aria-activedescendant="listbox-item-3" aria-labelledby="listbox-label"
+            class="max-h-56 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+            role="listbox"
             tabindex="-1">
-          <li :key="item.id" :value="item.id" @click="switchItem(item)" class="text-gray-900 hover:text-white hover:bg-orange-400 cursor-pointer select-none relative py-2 pl-3 pr-9" id="listbox-item-0"
+          <li :key="item.id" :value="item.id" @click="switchItem(item)"
+              class="text-gray-900 hover:text-white hover:bg-orange-400 cursor-pointer select-none relative py-2 pl-3 pr-9"
+              id="listbox-item-0"
               role="option"
               v-for="item in values">
             <div class="flex items-center space-x-3">
               <div :style="`background-color: ${item.color};`" class="w-2 h-2 rounded-full"></div>
-              <span :class="{ 'font-semibold': selected.includes(item) }" class="font-normal block truncate">{{ item.name }}</span>
+              <span :class="{ 'font-semibold': containsObject(labels, item.id) }" class="font-normal block truncate">{{ item.name }}</span>
             </div>
-            <span class="absolute inset-y-0 right-0 flex items-center pr-4" v-if="selected.includes(item)">
-                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path clip-rule="evenodd"
-                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                          fill-rule="evenodd"/>
-                                </svg>
-                            </span>
+            <span class="absolute inset-y-0 right-0 flex items-center pr-4" v-if="containsObject(labels, item.id)">
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                <path clip-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      fill-rule="evenodd"/>
+              </svg>
+            </span>
           </li>
         </ul>
       </div>
@@ -49,26 +49,29 @@
 <script>
   export default {
     name: "Dropdown",
-    props: ["values"],
+    props: ["values", "selected"],
     data: () => ({
       open: false,
-      selected: []
+      labels: []
     }),
+    created() {
+      this.labels = this.selected
+    },
     methods: {
-      // updateValue: function (value) {
-      //     const selected = this.values.filter(label => label.id === parseInt(value))[0]
-      //     this.$emit('input', selected)
-      // }
       switchItem(value) {
-        if (this.selected.includes(value))
-          this.$delete(this.selected, this.selected.indexOf(value))
+        if (this.containsObject(this.labels, value.id))
+          this.labels.splice(this.labels.findIndex(e => e.id === value.id), 1);
         else
-          this.selected.push(value)
+          this.labels.push(value);
 
-        this.$emit('update', this.selected)
+        this.$emit("input", this.labels)
       },
       away() {
         this.open = false
+      },
+      containsObject(list, id) {
+        return !!list.some(e => e.id === id);
+
       }
     }
   }
