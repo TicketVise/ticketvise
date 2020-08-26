a54d2ae36067eb5a0da8d4e760ec716676cd5962
IvandeWolf <ivan.dewolf1997@gmail.com>
2020-08-26 22:46:36 +0200

    Last settings changes

--- frontend/components/inbox_statistics/InboxStatistics.vue
+++ frontend/components/inbox_statistics/InboxStatistics.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="flex flex-col mb-3 space-y-4">
-    <div class="flex space-x-4">
-      <card outlined class="w-1/2 p-3">
+  <div>
+    <h3 class="text-lg leading-6 font-medium text-gray-900 px-4 pb-1 pt-4">
+      Graphs
+    </h3>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 mb-2 gap-4">
+      <div class="shadow p-3">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
           Number of tickets per day
         </h3>
         <tickets-chart/>
-      </card>
-      <card outlined class="w-1/2 p-3">
+      </div>
+      <div class="shadow p-3">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
           Number of tickets per hour
         </h3>
         <tickets-chart type="hour"/>
-      </card>
-    </div>
-    <div class="flex space-x-4">
-      <card outlined class="w-1/2 p-3">
+      </div>
+      <div class="shadow p-3">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
           Average response time
         </h3>
         <agent-response-time-chart/>
-      </card>
-      <card outlined class="w-1/2 p-3">
+      </div>
+      <div class="shadow p-3">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
           Label usage
         </h3>
         <labels-chart/>
-      </card>
+      </div>
     </div>
   </div>
 </template>
