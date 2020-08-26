8ecbde6d66a0111d7d4738798be1059b3cb6d2de
Ruben Jonk <ruben.jonk@hotmail.nl>
2020-08-26 18:17:54 +0200

    text color chip based on background

--- frontend/components/elements/chip/Chip.vue
+++ frontend/components/elements/chip/Chip.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-700"
-        style="line-height: 1 !important;" :style="bgColor">
+        :style="style" style="line-height: 1 !important;">
     <slot></slot>
   </span>
 </template>
@@ -16,9 +16,23 @@
     },
     data() {
       return {
-        bgColor: {
-          backgroundColor: this.background
+        style: {
+          backgroundColor: this.background,
+          color: this.getFontColor(this.background)
+        },
+      }
+    },
+    methods: {
+      getFontColor(hexcolor) {
+        if (hexcolor.slice(0, 1) === '#') {
+          hexcolor = hexcolor.slice(1);
         }
+
+        let r = parseInt(hexcolor.substr(0, 2), 16);
+        let g = parseInt(hexcolor.substr(2, 2), 16);
+        let b = parseInt(hexcolor.substr(4, 2), 16);
+        let brightness = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        return (brightness >= 128) ? "#374151" : "#ffffff";
       }
     }
   }
