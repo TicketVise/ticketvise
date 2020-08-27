7dab35c724e4fb8539259d13ff30b126cf563118
IvandeWolf <ivan.dewolf1997@gmail.com>
2020-08-27 11:30:13 +0200

    Fixed test delete inbox label

--- backend/ticketvise/views/inbox/labels.py
+++ backend/ticketvise/views/inbox/labels.py
@@ -78,3 +78,11 @@ class DeleteInboxLabelView(LabelCoordinatorRequiredMixin, DeleteView):
 
     def get_success_url(self):
         return reverse("inbox_labels", args=(self.kwargs["inbox_id"],))
+
+    def get_context_data(self, **kwargs):
+        inbox = get_object_or_404(Inbox, pk=self.kwargs.get("inbox_id"))
+
+        context = super(DeleteView, self).get_context_data(**kwargs)
+        context['inbox'] = inbox
+
+        return context
