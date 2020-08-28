e24a972fc5098a4d8b9bd7b0696be648fe21d67d
Ruben Jonk <ruben.jonk@hotmail.nl>
2020-08-28 11:49:31 +0200

    assignee overview bug fix

--- frontend/components/ticket_overview/TicketCard.vue
+++ frontend/components/ticket_overview/TicketCard.vue
@@ -5,7 +5,7 @@
     </span>
 
     <h3 v-if="!small" class="text-xs text-gray-500">{{ date(ticket.date_created) }}</h3>
-    <h3 class="text-xs text-gray-500" v-if="!small && ticket.assignee && ticket.assignee.name">Assignee: {{
+    <h3 class="text-xs text-gray-500" v-if="!small && ticket.assignee && ticket.assignee.username">Assignee: {{
       ticket.assignee.first_name }} {{ ticket.assignee.last_name }}</h3>
     <h3 class="text-xs text-gray-500" v-else-if="!small">Assignee: None</h3>
 
