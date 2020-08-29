6607a1792a9f7012d02918154b8d84df605379fd
Ruben Jonk <ruben.jonk@hotmail.nl>
2020-08-29 16:32:10 +0200

    LabelDropdown.vue test

--- frontend/tests/components/Chip.test.js
+++ frontend/tests/components/Chip.test.js
@@ -1,9 +1,9 @@
 import Chip from "@/elements/chip/Chip";
 import {mount} from '@vue/test-utils'
 
-const source = "../../backend/ticketvise/static/img/avatars/default-avatar.png";
+test("chip has dot", () => {
+  const wrapper = mount(Chip, {propsData: {background: "#FF6600"}});
 
-test("avatar is square", () => {
-  const wrapper = mount(Avatar, {propsData: {source: source}});
+  expect(wrapper.findAll("div").at(1).html()).toBe("<div class=\"w-2 h-2 rounded-full flex-col mr-1\" style=\"background-color: rgb(255, 102, 0);\"></div>")
 
 });
