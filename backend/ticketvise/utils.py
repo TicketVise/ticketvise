3fb48cee8da4fd94532410d337effceda7fc75fd
Tom J. Wassing <tjwassing@gmail.com>
2020-09-09 21:50:08 +0200

    Implemented default random color and removed unsued context_processor

--- backend/ticketvise/utils.py
+++ backend/ticketvise/utils.py
@@ -3,12 +3,13 @@ Utils
 -------------------------------
 Various utility functions for use in python files.
 """
+import random
 
 from PIL import ImageColor
 
-from .models.ticket import Status
-from .models.user import Role
-from .settings import DEFAULT_AVATAR_PATH
+
+def random_color():
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 def crop_image(image):
@@ -49,33 +50,3 @@ def get_text_color(background_color):
     r, g, b = ImageColor.getcolor(background_color, "RGB")
     return "#374151" if (r * 0.299 + g * 0.587 + b * 0.114) > 186 else "#ffffff"
 
-
-def get_global_context():
-    """
-    Obtain the global context.
-
-    :return: Global context template variables.
-    :rtype: dict
-    """
-    return {
-        "DEFAULT_AVATAR_PATH": DEFAULT_AVATAR_PATH,
-        "ROLE_STUDENT": Role.GUEST,
-        "ROLE_ASSISTANT": Role.AGENT,
-        "ROLE_COORDINATOR": Role.MANAGER,
-        "STATUS_PENDING": Status.PENDING,
-        "STATUS_ASSIGNED": Status.ASSIGNED,
-        "STATUS_ANSWERED": Status.ANSWERED,
-        "STATUS_CLOSED": Status.CLOSED,
-    }
-
-
-def add_global_context(context_dict):
-    """
-    Update the context dictionary.
-
-    :param dict context_dict: The global context.
-
-    :return: Context dictionary updated with global context.
-    :rtype: dict
-    """
-    context_dict.update(get_global_context())
