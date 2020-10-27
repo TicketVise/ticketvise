d266e10d3271fdddbde067cbae3b7e4fb667fa21
IvandeWolf <ivan.dewolf1997@gmail.com>
2020-10-27 14:27:18 +0100

    Added the functionality to select the workgroup scheduling algorithm

--- backend/ticketvise/models/inbox.py
+++ backend/ticketvise/models/inbox.py
@@ -25,6 +25,8 @@ class SchedulingAlgorithm(models.TextChoices):
     ROUND_ROBIN = ("round-robin", "Round Robin")
     #: Assigns tickets to the assistant with the least assigned tickets.
     LEAST_ASSIGNED_FIRST = ("least-assigned-first", "Least Assigned First")
+    # Schedule tickets based on sections
+    SECTIONS = ("sections", "Workgroup")
     # Schedule all to one assistant
     FIXED = ("fixed", "Fixed")
 
