2ff6e406abe909d47405b54cb4b432c995f14cf7
Tom J. Wassing <tjwassing@gmail.com>
2020-11-01 16:13:46 +0100

    Migrated statistcs to Vue

--- backend/ticketvise/views/inbox/statistics.py
+++ backend/ticketvise/views/inbox/statistics.py
@@ -17,48 +17,6 @@ class InboxStatisticsView(InboxCoordinatorRequiredMixin, TemplateView):
         inbox = get_object_or_404(Inbox, pk=self.kwargs.get("pk"))
 
         context = super().get_context_data(**kwargs)
-
         context['inbox'] = inbox
-        context['total_tickets'] = Ticket.objects.filter(inbox=inbox).count()
-        context['total_students'] = UserInbox.objects.filter(inbox=inbox, role=Role.GUEST).count()
-        context['avg_response_time'] = get_average_response_time(inbox)
 
-        now = datetime.now()
-        last_7_days = now - timedelta(days=7)
-        context['current_week_pending'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                           date_created__gte=last_7_days,
-                                                                           old_status=Status.PENDING).count()
-        context['current_week_assigned'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                            date_created__gte=last_7_days,
-                                                                            new_status=Status.ASSIGNED).count()
-        context['current_week_answered'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                            date_created__gte=last_7_days,
-                                                                            new_status=Status.ANSWERED).count()
-        context['current_week_closed'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                          date_created__gte=last_7_days,
-                                                                          new_status=Status.CLOSED).count()
-
-        last_14_days = now - timedelta(days=14)
-        context['last_week_pending'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                        date_created__lte=last_7_days,
-                                                                        date_created__gte=last_14_days,
-                                                                        old_status=Status.PENDING).count()
-        context['last_week_assigned'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                         date_created__lte=last_7_days,
-                                                                         date_created__gte=last_14_days,
-                                                                         new_status=Status.ASSIGNED).count()
-        context['last_week_answered'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                         date_created__lte=last_7_days,
-                                                                         date_created__gte=last_14_days,
-                                                                         new_status=Status.ANSWERED).count()
-        context['last_week_closed'] = TicketStatusEvent.objects.filter(ticket__inbox=inbox,
-                                                                       date_created__lte=last_7_days,
-                                                                       date_created__gte=last_14_days,
-                                                                       new_status=Status.CLOSED).count()
-
-        context['last_week_total_tickets'] = Ticket.objects.filter(inbox=inbox, date_created__lte=last_7_days).count()
         return context
-
-
-def calculate_increase(current_week, last_week):
-    return round((current_week / last_week) * 100, 2) if last_week else 0
