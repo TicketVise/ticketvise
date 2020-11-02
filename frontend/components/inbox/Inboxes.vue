780077f67c380d72d0aac064e20c49f439b8d0a4
IvandeWolf <ivan.dewolf1997@gmail.com>
2020-11-02 12:54:34 +0100

    codestyle issues fixed

--- frontend/components/inbox/Inboxes.vue
+++ frontend/components/inbox/Inboxes.vue
@@ -1,44 +1,41 @@
 <template>
   <div>
-      <header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold leading-tight text-gray-900">
-        Your inboxes
-      </h1>
-    </div>
-  </header>
-  <main>
-    <div class="max-w-7xl mx-auto sm:py-6 sm:px-6 lg:px-8">
-      <div v-if="userInboxes" class="px-4 py-6 sm:px-0 grid md:grid-cols-3 gap-4">
-        <div v-for="userInbox in userInboxes" class="bg-white relative">
-          <a :href="userInbox.inbox.id" class="" aria-label="more options">
+    <header class="bg-white shadow">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold leading-tight text-gray-900">
+          Your inboxes
+        </h1>
+      </div>
+    </header>
+    <main>
+      <div class="max-w-7xl mx-auto sm:py-6 sm:px-6 lg:px-8">
+        <div v-if="userInboxes" class="px-4 py-6 sm:px-0 grid md:grid-cols-3 gap-4">
+          <div :key="userInbox.inbox.id" v-for="userInbox in userInboxes" class="bg-white relative">
+            <a :href="userInbox.inbox.id" class="" aria-label="more options">
               <div class="h-64 w-full rounded hover:shadow-lg transition-shadow ease-in-out duration-100"
-                   :style="{'background-color': userInbox.inbox.color}">
+                  :style="{'background-color': userInbox.inbox.color}">
                 <img class="h-64 w-full rounded object-cover opacity-75" :src="userInbox.inbox.image"
-                     :alt="userInbox.inbox.name">
+                    :alt="userInbox.inbox.name">
               </div>
-          </a>
+            </a>
 
-          <div
-              class="absolute bottom-0 bg-gray-800 bg-opacity-50 w-full text-white flex py-2 px-4 space-x-2 rounded-br rounded-bl shadow-lg"
-              style="overflow: hidden; text-overflow: ellipsis;">
-              <button type="button" @click="onBookmarkClick()" class="pl-0">
+            <div class="absolute bottom-0 bg-gray-800 bg-opacity-50 w-full text-white flex py-2 px-4 space-x-2 rounded-br rounded-bl shadow-lg"
+                style="overflow: hidden; text-overflow: ellipsis;">
+              <button type="button" @click="onBookmarkClick()" class="pl-0 focus:outline-none">
                 <i v-if="userInbox.is_bookmarked" class="fa fa-bookmark"></i>
                 <i v-else class="fa fa-bookmark-o"></i>
               </button>
 
-            <a :href="userInbox.inbox.id">
-              {{ userInbox.inbox.name }}
-            </a>
+              <a :href="userInbox.inbox.id">{{ userInbox.inbox.name }}</a>
+            </div>
           </div>
         </div>
+        <div v-else class="flex flex-col items-center w-full">
+          <img src="/static/img/svg/undraw_empty_street_sfxm.svg" alt="Nothing here" class="w-1/2 md:w-1/3 mx-auto py-8">
+          <span class="text-gray-600 text-lg md:text-xl">You have no inboxes</span>
+        </div>
       </div>
-      <div v-else class="flex flex-col items-center w-full">
-        <img src="/static/img/svg/undraw_empty_street_sfxm.svg" alt="Nothing here" class="w-1/2 md:w-1/3 mx-auto py-8">
-        <span class="text-gray-600 text-lg md:text-xl">You have no inboxes</span>
-      </div>
-    </div>
-  </main>
+    </main>
   </div>
 </template>
 
@@ -56,7 +53,7 @@ export default {
   },
   methods: {
     onBookmarkClick: function () {
-
+      console.log('test')
     }
   }
 }
