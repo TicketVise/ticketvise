e699b5e6ea2976cc4cfc14a5280824bf5096dec8
Tom J. Wassing <tjwassing@gmail.com>
2020-11-20 12:04:26 +0100

    Fixed create ticket by email

--- backend/ticketvise/email/smtp.py
+++ backend/ticketvise/email/smtp.py
@@ -11,7 +11,7 @@ from ticketvise import settings
 from ticketvise.models.comment import Comment
 from ticketvise.models.inbox import Inbox
 from ticketvise.models.ticket import Ticket
-from ticketvise.models.user import User
+from ticketvise.models.user import User, UserInbox
 
 
 class SmtpServer:
@@ -23,13 +23,26 @@ class SmtpServer:
     def parse_message_id(self, raw_message_id):
         parts = raw_message_id.split("@")
         if len(parts) > 1:
-            return UUID(parts[0].replace("<", ""))
+            try:
+                return UUID(parts[0].replace("<", ""))
+            finally:
+                return None
 
         return None
 
+    def get_ticket(self, message):
+        if "Message-ID" not in message:
+            return None, None
+
+        message_id = self.parse_message_id(message["Message-ID"])
+        if not message_id:
+            return None, None
+
+        return Ticket.objects.filter(
+            Q(reply_message_id=message_id) | Q(comment_message_id=message_id)).first(), message_id
+
     @sync_to_async
     def handle_RCPT(self, server, session, envelope, address, rcpt_options):
-        print("receipt")
         if not Inbox.objects.filter(email=address).exists():
             return '550 not relaying to that domain'
 
@@ -39,7 +52,6 @@ class SmtpServer:
 
     @sync_to_async
     def handle_DATA(self, server, session, envelope):
-        print("data")
         mail_from = envelope.mail_from
 
         message = BytesParser(policy=policy.default).parsebytes(envelope.content)
@@ -48,24 +60,26 @@ class SmtpServer:
             content = body.get_content()
             reply = EmailReplyParser.parse_reply(content)
             author, _ = User.objects.get_or_create(email=mail_from)
+            ticket, message_id = self.get_ticket(message)
 
-            if "Message-ID" in message:
-                messageId = self.parse_message_id(message["Message-ID"])
-                if not messageId:
-                    return '501 Invalid Message-ID format'
-
-                ticket = Ticket.objects.get(Q(reply_message_id=messageId) | Q(comment_message_id=messageId))
+            if ticket:
                 if not ticket.inbox.enable_reply_by_email:
                     return '450 Reply by email is disabled for the inbox'
 
-                Comment.objects.create(ticket=ticket, author=author, is_reply=ticket.reply_message_id == messageId,
+                Comment.objects.create(ticket=ticket, author=author, is_reply=ticket.reply_message_id == message_id,
                                        content=reply)
+                UserInbox.objects.create(user=author, inbox=ticket.inbox)
+
+
             else:
-                inbox = Inbox.objects.get(email__in=envelope.rcpt_tos,)
+                inbox = Inbox.objects.get(email__in=envelope.rcpt_tos, )
+                print(inbox.enable_create_new_ticket_by_email)
                 if not inbox.enable_create_new_ticket_by_email:
                     return '450 Creation of ticket by email is disabled for the inbox'
 
+                print("create ticket")
                 Ticket.objects.create(author=author, inbox=inbox, title=message["Subject"], content=reply)
+                UserInbox.objects.create(user=author, inbox=inbox)
 
         return '250 OK'
 
