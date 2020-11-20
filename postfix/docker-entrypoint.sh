dce5bceed2ac8e2a6a27770e67be84c90645016b
Tom J. Wassing <tjwassing@gmail.com>
2020-11-20 11:31:27 +0100

    Implemented postfix as email gateway

--- postfix/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-postconf -e smtputf8_enable="no" \
-  mynetworks="0.0.0.0/0" \
-  inet_interfaces="all" \
-  mydomain="$DOMAIN" \
-  mynetworks="127.0.0.0/8, 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16"
-
-exec "$@"
