cd0b1006433f713785e1c3b4aa4ee42132a022e1
Ruben Jonk <ruben.jonk@hotmail.nl>
2020-12-27 10:34:50 +0100

    Added a new mixin for attachments

--- backend/ticketvise/views/api/security.py
+++ backend/ticketvise/views/api/security.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import AccessMixin
 from django.shortcuts import get_object_or_404
 
 from ticketvise.models.inbox import Inbox
-from ticketvise.models.ticket import Ticket
+from ticketvise.models.ticket import Ticket, TicketAttachment
 
 
 class UserIsInInboxMixin(AccessMixin):
@@ -80,6 +80,36 @@ class UserIsTicketAuthorOrInboxStaffMixin(AccessMixin):
 
         ticket = get_object_or_404(Ticket, inbox_id=inbox_id, ticket_inbox_id=ticket_inbox_id)
         if not (request.user.id == ticket.author.id or request.user.is_assistant_or_coordinator(ticket.inbox)):
+            return self.handle_no_permission()
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class UserIsAttachmentUploaderOrInboxStaffMixin(AccessMixin):
+    inbox_key = "inbox_id"
+    ticket_key = "ticket_inbox_id"
+    attachment_key = "pk"
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+
+        inbox_id = kwargs.get(self.inbox_key)
+        if not inbox_id:
+            return self.handle_no_permission()
+
+        ticket_inbox_id = kwargs.get(self.ticket_key)
+        if not ticket_inbox_id:
+            return self.handle_no_permission()
+
+        ticket = get_object_or_404(Ticket, inbox_id=inbox_id, ticket_inbox_id=ticket_inbox_id)
+        if not (request.user.id == ticket.author.id or request.user.is_assistant_or_coordinator(
+                ticket.inbox) or ticket.shared_with.filter(pk=request.user.id).exists()):
+            return self.handle_no_permission()
+
+        attachment_id = kwargs.get(self.attachment_key)
+        attachment = get_object_or_404(TicketAttachment, pk=attachment_id)
+        if not (request.user.id == attachment.uploader.id or request.user.is_assistant_or_coordinator(ticket.inbox)):
             return self.handle_no_permission()
 
         return super().dispatch(request, *args, **kwargs)
