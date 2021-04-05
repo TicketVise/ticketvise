160834a6604630badf4fceae1b40ed07996723c1
Ruben Jonk <ruben.jonk@hotmail.nl>
2021-04-05 21:06:18 +0200

    Add public ticket checkbox to form

--- frontend/components/ticket/TicketForm.vue
+++ frontend/components/ticket/TicketForm.vue
@@ -11,12 +11,12 @@
         </label>
         <error class="mb-2" v-for="error in this.errors.title" :key="error" :message="error"></error>
         <input
-          class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
-          id="title"
-          name="title"
-          placeholder="Give your question a title"
-          type="text"
-          v-model="title"
+                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                id="title"
+                name="title"
+                placeholder="Give your question a title"
+                type="text"
+                v-model="title"
         >
       </div>
 
@@ -52,6 +52,33 @@
       <h4 class="block text-gray-700 font-bold mb-2">Attachments</h4>
       <error class="mb-2" v-for="error in this.errors.attachments" :key="error" :message="error"></error>
       <file-upload v-bind:value="files" v-on:input="setFiles" class="mb-4"></file-upload>
+
+      <div class="mt-5 md:mt-0 md:col-span-2 m-4">
+        <div class="mt-4 space-y-4">
+          <div class="flex items-start">
+            <div class="h-5 flex items-center">
+              <input id="is_public" name="is_public" type="checkbox" v-model="is_public"
+                     class="focus:ring-orange-500 h-4 w-4 text-orange-600 border border-gray-600 rounded">
+            </div>
+            <div class="ml-3 text-sm w-1/2">
+              <label for="is_public" class="font-medium text-gray-700">Public Ticket</label>
+              <p class="text-gray-500">Public Tickets can be seen by everyone in this inbox.</p>
+            </div>
+          </div>
+          <div class="flex items-start">
+            <div class="h-5 flex items-center">
+              <input id="is_anonymous" name="is_anonymous" type="checkbox" v-model="is_anonymous"
+                     class="focus:ring-orange-500 h-4 w-4 text-orange-600 border border-gray-600 rounded">
+            </div>
+            <div class="ml-3 text-sm w-1/2">
+              <label for="is_anonymous" class="font-medium text-gray-700">Anonymous Ticket</label>
+              <p class="text-gray-500">If a ticket is public and anonymous, the ticket, content and attachments will be
+                visible, but your name will be hidden to fellow students. The staff can still see your name.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
       <submit-button v-on:click.native="submit" text="Submit" class="bg-primary hover:bg-orange-500 text-white">
         <i class="fa fa-paper-plane mr-2"></i>
@@ -101,6 +128,8 @@
         const formData = new FormData();
         formData.append("content", content);
         formData.append("title", this.title);
+        formData.append("is_public", this.is_public);
+        formData.append("is_anonymous", this.is_anonymous);
 
         this.labels.forEach(label => formData.append("labels", label.id))
         this.files.forEach(file => formData.append("files", file))
