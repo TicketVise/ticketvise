ef114a3b35586896329d8f776f6b385a42bc2563
IvandeWolf <ivan.dewolf1997@gmail.com>
2021-08-06 12:04:46 +0200

    Tests

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -4,9 +4,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    expect(msg).toEqual(msg)
   })
 })
