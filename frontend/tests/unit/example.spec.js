04c375e71888c1d9a176ad2e0bd21fd636d61162
IvandeWolf <ivan.dewolf1997@gmail.com>
2021-08-06 12:08:54 +0200

    Update

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,6 +1,3 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
-
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
