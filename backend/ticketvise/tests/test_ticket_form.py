5eca0ca733371c4535bb16816072b1940d968d6c
IvandeWolf <ivan.dewolf1997@gmail.com>
2021-08-19 12:29:26 +0200

    Json dumps

--- backend/ticketvise/tests/test_ticket_form.py
+++ backend/ticketvise/tests/test_ticket_form.py
@@ -3,6 +3,7 @@ Test Ticket Form
 -------------------------------
 This file tests the ticket form page which creates a ticket.
 """
+import json
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APITestCase
 
@@ -23,7 +24,7 @@ class TicketFormTestAPI(APITestCase, TicketTestCase):
             "is_public": None
         }
 
-        response = self.client.post(url, data, content_type="application/json")
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, 201)
         data["ticket_inbox_id"] = response.data["ticket_inbox_id"]
         self.assertDictEqual(response.data, data)
@@ -43,7 +44,7 @@ class TicketFormTestAPI(APITestCase, TicketTestCase):
             "is_public": None
         }
 
-        response = self.client.post(url, data, content_type="application/json")
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_create_ticket_shared(self):
@@ -59,7 +60,7 @@ class TicketFormTestAPI(APITestCase, TicketTestCase):
             "is_public": None
         }
 
-        response = self.client.post(url, data, content_type="application/json")
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
         data["ticket_inbox_id"] = response.data["ticket_inbox_id"]
         self.assertDictEqual(response.data, data)
@@ -78,6 +79,6 @@ class TicketFormTestAPI(APITestCase, TicketTestCase):
             "is_public": None
         }
 
-        response = self.client.post(url, data, content_type="application/json")
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {"shared_with": ["This ticket cannot be shared with one of these users"]})
