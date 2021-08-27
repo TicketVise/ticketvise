aaa246e2085b1799f80df573c51a854ec7795d7e
Tom J. Wassing <tjwassing@gmail.com>
2021-08-27 16:42:53 +0200

    save

--- backend/ticketvise/mail/__init__.py
+++ backend/ticketvise/mail/__init__.py
@@ -1,6 +1,5 @@
-
 import base64
 
 def oauth_2_auth_base64(username, access_token):
-    auth_string = f"user={username}\1auth=Bearer {access_token}\1\1"
-    return base64.b64encode(auth_string)
+    auth_string = 'user=%s\1auth=Bearer %s\1\1' % (username, access_token)
+    return base64.b64encode(auth_string.encode("ascii"))
