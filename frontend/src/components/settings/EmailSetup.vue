3318ddd60a1052b139419de994681c3202df3b06
root <root@lydi1-nb144825.localdomain>
2021-10-04 11:34:41 +0200

    Added spinner en api call

--- frontend/src/components/settings/EmailSetup.vue
+++ frontend/src/components/settings/EmailSetup.vue
@@ -20,13 +20,13 @@
             Provide your email address and password to connect your mailbox to your TicketVise inbox.
           </p>
         </div>
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" @submit.prevent="submit()">
           <div>
             <label for="email" class="block text-sm font-medium text-gray-700">
               Email address
             </label>
             <div class="mt-1">
-              <input id="email" name="email" type="email" autocomplete="email" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+              <input id="email" name="email" type="email" autocomplete="email" v-model="email" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
             </div>
           </div>
 
@@ -35,14 +35,18 @@
               Password
             </label>
             <div class="mt-1">
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+              <input id="password" name="password" type="password" v-model="password" autocomplete="current-password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
             </div>
           </div>
             <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-              <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:col-start-2 sm:text-sm" @click="close()">
+              <button :disabled="loading" type="submit" :class="{'cursor-not-allowed': loading}" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:col-start-2 sm:text-sm">
+                <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
                 Sign in
               </button>
-              <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm" @click="close()">
+              <button :disabled="loading" type="button" :class="{'cursor-not-allowed': loading}" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm" @click="close()">
                 Cancel
               </button>
             </div>
@@ -53,7 +57,7 @@
 </template>
 
 <script>
-//   import axios from 'axios'
+import axios from 'axios'
 
 const Mailbox = require('@/assets/img/svg/mailbox.svg')
 
@@ -61,12 +65,27 @@ const Mailbox = require('@/assets/img/svg/mailbox.svg')
     name: 'EmailSetup',
     data () {
       return {
-        mailboxImage: Mailbox
+        mailboxImage: Mailbox,
+        email: "",
+        password: "",
+        loading: false
       }
     },
     methods: {
       close() {
         this.$emit('close')
+      },
+      submit() {
+        const inboxId = this.$route.params.inboxId
+        const data = {
+            email: this.email,
+            password: this.password
+        }
+
+        this.loading = true
+        axios.post(`/api/inboxes/${ inboxId }/email/login`, data).then(() => {
+            this.email = this.password = ''
+        })
       }
     }
   }
