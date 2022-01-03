76df9aeb0aab55b5b6ca2ca6bc3c3f65e0aca654
Tom J. Wassing <tjwassing@gmail.com>
2022-01-03 10:24:50 +0100

    Removed unused code

--- backend/ticketvise/config.py
+++ backend/ticketvise/config.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class TicketViseConfig(AppConfig):
     name = "ticketvise"
-
-    def ready(self):
-        from ticketvise.tasks import start_schedule
-        pass
-        # start_schedule()
