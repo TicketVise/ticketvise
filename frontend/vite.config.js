7780670c2dccb86db3ab2696f43e735a84415a7a
Tom J. Wassing <tjwassing@gmail.com>
2022-03-04 15:39:25 +0100

    Fixed serving

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: '8080'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
