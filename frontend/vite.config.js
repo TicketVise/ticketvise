b5677931125175ca667bd32603bd578dc9c3dead
Tom J. Wassing <tjwassing@gmail.com>
2022-03-09 16:49:22 +0100

    Added JSX support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), vueJsx(), svgLoader()],
   server: {
     host: true,
     port: 8080,
