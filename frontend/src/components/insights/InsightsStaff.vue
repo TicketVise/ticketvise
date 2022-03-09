4d6747c2116e52b9777b4d945947f0c01df40964
IvandeWolf <ivan.dewolf1997@gmail.com>
2022-03-09 17:03:32 +0100

    Some changes

--- frontend/src/components/insights/InsightsStaff.vue
+++ frontend/src/components/insights/InsightsStaff.vue
@@ -2,84 +2,70 @@
   <section aria-labelledby="tickets-heading">
     <h2 id="tickets-heading" class="sr-only">Ticket insights</h2>
 
-    <!-- Some basic stats first -->
-    <dl class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-      <div v-for="item in stats" :key="item.name" class="px-4 py-3 rounded-lg border">
-        <dt class="text-base font-normal text-gray-900">
-          {{ item.name }}
-        </dt>
-        <dd class="flex justify-between items-baseline md:block lg:flex">
-          <div class="flex items-baseline text-2xl font-semibold text-primary-600">
-            {{ item.stat }}
-            <span v-if="item.previousStat" class="ml-2 text-sm font-medium text-gray-500"> from {{ item.previousStat }} </span>
-          </div>
-
-          <div v-if="item.change" :class="[item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800', 'inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0']">
-            <ArrowSmUpIcon v-if="item.changeDirection === 'up'" class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5" :class="[item.changeType === 'increase' ? 'text-green-500' : 'text-red-500']" aria-hidden="true" />
-            <ArrowSmDownIcon v-else class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5" :class="[item.changeType === 'increase' ? 'text-green-500' : 'text-red-500']" aria-hidden="true" />
-            <span class="sr-only"> {{ item.changeType === 'increase' ? 'Increased' : 'Decreased' }} by </span>
-            {{ item.change }}
-          </div>
-        </dd>
-      </div>
-    </dl>
-
-    <!-- Some general graphs here -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
-      <div class="col-span-1 border rounded-lg p-4 pt-2">
-        <h2 class="text-lg font-semibold text-primary-600">Helpfulness of staff</h2>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+      <!-- Imformation about the staff. -->
+      <div class="col-span-1 sm:col-span-2 border rounded-lg p-4 pt-2">
+        <h2 class="text-lg font-semibold text-primary-600">Staff overview</h2>
         <ul role="list" class="divide-y divide-gray-200">
           <li v-for="member in staff.sort((a, b) => b.helpfulness.percentage - a.helpfulness.percentage)" :key="member.id">
             <div class="p-3">
-              <div class="flex items-center justify-between">
-                <span class="inline-flex items-center px-2 py-0.5 rounded-full text-sm text-gray-800 truncate space-x-2">
-                  <img class="inline-block h-6 w-6 rounded-full" :src="member.image" alt="" />
-                  <span class="truncate">{{ member.name }}</span>
+              <div class="grid grid-cols-8 gap-2 items-center justify-between">
+                <span class="col-span-4 inline-flex items-center px-2 rounded-full text-sm text-gray-800 truncate space-x-2">
+                  <img class="inline-block h-8 w-8 rounded-full mr-2" :src="member.image" alt="" />
+                  <div class="flex flex-col">
+                    <span class="truncate text-base font-semibold leading-none">{{ member.name }}</span>
+                    <div class="space-x-2">
+                      <span class="text-gray-600 leading-none">Tickets: {{ member.helpfulness.count + Math.floor(Math.random() * 10) }}</span>
+                      <span class="text-gray-600 leading-none">Response time: {{ (Math.random() * 10).toPrecision(2) }}h</span>
+                    </div>
+                  </div>
                 </span>
-                <div class="ml-2 flex-shrink-0 flex">
-                  <p class="px-2 inline-flex leading-5 font-medium rounded-full text-primary text-xl items-baseline">
-                    {{ member.helpfulness.percentage }}
-                    <span class="text-gray-400 text-xs font-normal">/{{ member.helpfulness.count }}</span>
-                  </p>
-                </div>
+                <span class="col-span-2 inline-flex ml-auto items-center px-2 py-0.5 rounded text-xs font-medium bg-primary-100 text-primary-700 w-full">
+                  <BookmarkIcon class="w-4 h-4 mr-1" />
+                  <span class="flex-shrink-0 truncate">{{ member.mainTopic }}</span>
+                </span>
+                <p class="col-span-2 px-2 inline-flex ml-auto leading-5 font-medium rounded-full text-primary text-xl items-baseline">
+                  <span>{{ member.helpfulness.percentage }}%</span>
+                  <span class="text-gray-400 text-xs font-normal">/{{ member.helpfulness.count }}</span>
+                  <ThumbUpIcon class="w-5 h-5 ml-1 self-end" />
+                </p>
               </div>
             </div>
           </li>
         </ul>
       </div>
 
-      <div class="col-span-1 border rounded-lg p-4 pt-2">
-        <h2 class="text-lg font-semibold text-primary-600">Most answered topic</h2>
-        <ul role="list" class="divide-y divide-gray-200">
-          <li v-for="member in staff" :key="member.id">
-            <div class="p-3">
-              <div class="flex items-center justify-between">
-                <span class="inline-flex items-center px-2 py-0.5 rounded-full text-sm text-gray-800 truncate space-x-2">
-                  <img class="inline-block h-6 w-6 rounded-full" :src="member.image" alt="" />
-                  <span class="truncate">{{ member.name }}</span>
-                </span>
-                <div class="ml-2 flex-shrink-0 flex">
-                  <span class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-primary-100 text-primary-700">
-                    {{ member.mainTopic }}
-                  </span>
-                </div>
-              </div>
+      <!-- Some basic stats -->
+      <dl class="flex space-x-4 col-span-1 sm:col-span-2 w-full items-start">
+        <div v-for="item in stats" :key="item.name" class="px-4 py-3 rounded-lg border w-full">
+          <dt class="text-base font-normal text-gray-900">
+            {{ item.name }}
+          </dt>
+          <dd class="flex justify-between items-baseline md:block lg:flex">
+            <div class="flex items-baseline text-2xl font-semibold text-primary-600">
+              {{ item.stat }}
+              <span v-if="item.previousStat" class="ml-2 text-sm font-medium text-gray-500"> from {{ item.previousStat }} </span>
             </div>
-          </li>
-        </ul>
-      </div>
+
+            <div v-if="item.change" :class="[item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800', 'inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0']">
+              <ArrowSmUpIcon v-if="item.changeDirection === 'up'" class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5" :class="[item.changeType === 'increase' ? 'text-green-500' : 'text-red-500']" aria-hidden="true" />
+              <ArrowSmDownIcon v-else class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5" :class="[item.changeType === 'increase' ? 'text-green-500' : 'text-red-500']" aria-hidden="true" />
+              <span class="sr-only"> {{ item.changeType === 'increase' ? 'Increased' : 'Decreased' }} by </span>
+              {{ item.change }}
+            </div>
+          </dd>
+        </div>
+      </dl>
     </div>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
-import { ArrowSmDownIcon, ArrowSmUpIcon } from '@heroicons/vue/solid'
+import { ArrowSmDownIcon, ArrowSmUpIcon, BookmarkIcon, ThumbUpIcon } from '@heroicons/vue/solid'
 
 const stats = [
-  { name: 'Total Staff', stat: '6', previousStat: '', change: '', changeDirection: '', changeType: 'increase' },
   { name: 'Average time to respond', stat: '2.3h', previousStat: '2.4h', change: '3.15%', changeDirection: 'down', changeType: 'increase' },
-  { name: 'Open Tickets', stat: '4', previousStat: '5', change: '20.00%', changeDirection: '', changeType: 'decrease' },
   { name: 'Average Tickets per user', stat: '2.4', previousStat: '', change: '', changeDirection: '', changeType: 'decrease' }
 ]
 
@@ -87,7 +73,9 @@ export default {
   name: 'InsightsStaff',
   components: {
     ArrowSmDownIcon,
-    ArrowSmUpIcon
+    ArrowSmUpIcon,
+    BookmarkIcon,
+    ThumbUpIcon
   },
   setup () {
     return {
@@ -100,7 +88,7 @@ export default {
   async mounted () {
     const { inboxId } = this.$route.params
 
-    const testTopics = ['Assignment', 'Lecture', 'Slides', 'Code']
+    const testTopics = ['Assignment 1 van het boek over meganica', 'Lecture', 'Slides', 'Code']
 
     /* Getting the staff data. */
     const staffResponse = await axios.get(`/api/inboxes/${inboxId}/staff`)
@@ -108,8 +96,8 @@ export default {
       return {
         name: member.first_name + ' ' + member.last_name,
         helpfulness: {
-          percentage: (Math.random() * 100).toPrecision(3),
-          count: Math.floor(Math.random() * 100)
+          percentage: (Math.random() * 50 + 50).toPrecision(3),
+          count: Math.floor(Math.random() * 90)
         },
         image: member.avatar_url,
         mainTopic: testTopics[Math.floor(Math.random() * testTopics.length)]
