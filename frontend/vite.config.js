1b58475927004d3d9dcf63a7168eda90021ec5e3
Tom J. Wassing <tjwassing@gmail.com>
2022-03-11 13:54:29 +0100

    removed vite-svg-loader

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), svgLoader()],
+  plugins: [vue(), vueJsx()],
   server: {
     host: true,
     port: 8080,
